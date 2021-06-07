af85fc32e2402d764e7b5da8f0cd5c85dabaf69f
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2021-06-07 18:30:06 +0200

    Do not use weekend days

--- src/main/kotlin/Time.kt
+++ src/main/kotlin/Time.kt
@@ -3,7 +3,7 @@ import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
 import kotlin.time.minutes
 
-@ExperimentalTime val delayMicro = 100.milliseconds
+@ExperimentalTime val delayMicro = 200.milliseconds
 @ExperimentalTime val delayMini = 1.minutes
 @ExperimentalTime val delayShort = 5.minutes
 @ExperimentalTime val delayLong = 30.minutes
@@ -12,5 +12,7 @@ fun isWorkingHours(): Boolean {
     val currentMoment: Instant = Clock.System.now()
     val tzBerlin = TimeZone.of("Europe/Berlin")
     val dateTime = currentMoment.toLocalDateTime(tzBerlin)
-    return dateTime.hour in (9..19)
+    val workingHours = dateTime.hour in (9..19)
+    val workingDays = dateTime.dayOfWeek !in listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+    return workingHours && workingDays
 }
