a3898487f549a044cc24083346b5c0b9f1d99152
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2021-12-23 17:45:46 +0100

    Upgrade kotlin and tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "1.4.32"
+	val kotlinVersion = "1.6.10"
 	kotlin("jvm") version kotlinVersion
 	kotlin("kapt") version kotlinVersion
 	kotlin("plugin.serialization") version kotlinVersion
+	id("com.github.ben-manes.versions") version "0.39.0"
 	application
 }
 
@@ -22,14 +23,14 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-	implementation("com.pi4j:pi4j-core:1.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+	implementation("com.pi4j:pi4j-core:1.4") // v2 has breaking changes
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
-	implementation("com.squareup.okhttp3:okhttp:4.9.0")
+	implementation("com.squareup.okhttp3:okhttp:4.9.3")
 	implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
 }
 
 tasks.withType<KotlinCompile> {
