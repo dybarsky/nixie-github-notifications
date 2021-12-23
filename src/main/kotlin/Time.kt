c6431cc0eab32227059f0816e9f6e29533ebb491
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2021-12-23 17:50:09 +0100

    Tune times

--- src/main/kotlin/Time.kt
+++ src/main/kotlin/Time.kt
@@ -1,18 +1,19 @@
 import kotlinx.datetime.*
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
-import kotlin.time.milliseconds
-import kotlin.time.minutes
 
 @ExperimentalTime val delayMicro = 200.milliseconds
-@ExperimentalTime val delayMini = 1.minutes
-@ExperimentalTime val delayShort = 5.minutes
+@ExperimentalTime val delayMini = 1.seconds
+@ExperimentalTime val delayShort = 1.minutes
 @ExperimentalTime val delayLong = 30.minutes
 
 fun isWorkingHours(): Boolean {
     val currentMoment: Instant = Clock.System.now()
     val tzBerlin = TimeZone.of("Europe/Berlin")
     val dateTime = currentMoment.toLocalDateTime(tzBerlin)
-    val workingHours = dateTime.hour in (9..19)
+    val workingHours = dateTime.hour in (9..21)
     val workingDays = dateTime.dayOfWeek !in listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
     return workingHours && workingDays
 }
